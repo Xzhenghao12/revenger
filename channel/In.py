2def09c7ad06f77d287318f1764da597665986fa
zhenghaoxiong <xzhxxh@163.com>
2026-05-07 17:34:30 +0800

    update OB

--- channel/In.py
+++ channel/In.py
@@ -7,14 +7,16 @@ acquire(time, fs) --> np.array
 
 import numpy as np
 import logging
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
+from plugins.useful_functions import find_port_by_manufacturer
+
 
 class NS: # 研究表明任天堂switch可以提高情侣之间的结婚效率
-    def __init__(self,
-                 fs: int):
-        self.fs = fs
+    def __init__(self):
+        self.fs = 1000
         self.if_married = False
 
-    def marry(self, time, fs) -> None:
+    def marry(self) -> None:
         try:
             print("TRYING TO GET MARRIED")
         except Exception as e:
@@ -22,7 +24,7 @@ class NS: # 研究表明任天堂switch可以提高情侣之间的结婚效率
         else:
             self.if_married = True
 
-    def divorce(self, time, fs) -> None:
+    def divorce(self) -> None:
         try:
             print("TRYING TO GET DIVORCE")
         except Exception as e:
@@ -35,3 +37,47 @@ class NS: # 研究表明任天堂switch可以提高情侣之间的结婚效率
 
         data = np.zeros(int(self.fs*time))
         return data
+
+class OB: # 关于OB(上帝)视角是否更适合玩游戏
+    def __init__(self):
+        self.if_married = False
+        self.params = BrainFlowInputParams()
+        self.board = None
+        self.fs = 250
+        self.board_id = BoardIds.CYTON_DAISY_BOARD
+        self.eeg_channels = BoardShim.get_eeg_channels(self.board_id)
+
+    def marry(self) -> None:
+        self.params.serial_port = find_port_by_manufacturer("FTDI")
+        if self.params.serial_port is None:
+            logging.error("NO FTDI PORT FOUND")
+            return
+        try:
+            print("TRYING TO GET MARRIED")
+            self.board = BoardShim(self.board_id, self.params)
+            self.fs = self.board.get_sampling_rate(self.board_id)
+            self.board.prepare_session()
+            self.board.start_stream()
+        except Exception as e:
+            logging.error(f"THOSE GUYS CANNOT BE TOGETHER: {e}")
+        else:
+            self.if_married = True
+
+    def divorce(self) -> None:
+        try:
+            print("TRYING TO GET DIVORCE")
+            self.board.stop_stream()
+            self.board.release_session()
+        except Exception as e:
+            logging.error(f"THOSE GUYS CANNOT BE SEPARATED: {e}")
+        else:
+            self.if_married = False
+
+    def acquire(self,
+                t: float) -> np.ndarray:
+        if not self.if_married:
+            return np.zeros(int(self.fs*t))
+        data = self.board.get_current_board_data(int(self.fs*t))[self.eeg_channels, :]
+        return data
+    
+
--- plugins/useful_functions.py
+++ plugins/useful_functions.py
@@ -1,4 +1,6 @@
 import inspect
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
+import serial.tools.list_ports
 
 def get_all_classes(module):
     """
@@ -14,3 +16,16 @@ def get_all_classes(module):
             class_dict[name] = obj
 
     return class_dict
+
+def find_port_by_manufacturer(target_manufacturer):
+    """
+    根据制造商名称查找对应的 COM 端口
+    :param target_manufacturer: 目标制造商名（如 "FTDI", "Silicon Labs"）
+    :return: 匹配到的端口号（如 "COM3"），未找到则返回 None
+    """
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        # manufacturer 有可能为 None，需要先判断
+        if port.manufacturer and target_manufacturer in port.manufacturer:
+            return port.device
+    return None
